753c111d45e546dc960176a77734e2d1b51dacad
Gilaine Young <gilaineyoung@gmail.com>
2023-11-20 15:51:26 +0000

    Moves Promise.reject from controller to model per PR comment

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -11,9 +11,6 @@ exports.getArticleById = (req, res, next) => {
     const { article_id } = req.params
     selectArticle(article_id)
     .then((article) => {
-        if (!article) {
-            return Promise.reject({ status: 404, msg: 'Article does not exist' })
-        }
         res.status(200).send({ article: article })
     })
     .catch(next)
--- models/app.models.js
+++ models/app.models.js
@@ -11,6 +11,9 @@ exports.selectArticle = (article_id) => {
     return db.query(`SELECT * FROM articles
         WHERE article_id = $1`, [+article_id])
     .then(({rows}) => {
+        if (!rows.length) {
+            return Promise.reject({ status: 404, msg: 'Article does not exist' })
+        }
         return rows[0]
     })
 }
