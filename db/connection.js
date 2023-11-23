8ecef53725159ed5f3119d079aa5e8f61c8caeda
Gilaine Young <gilaineyoung@gmail.com>
2023-11-23 13:44:29 +0000

    Updates connection.js for hosting setup

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
+const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-const ENV = process.env.NODE_ENV || 'development';
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
