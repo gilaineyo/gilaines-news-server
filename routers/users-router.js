8174121667b7e7c9fa3b5a033344e9d7bed300ff
Gilaine Young <gilaineyoung@gmail.com>
2023-11-24 14:47:57 +0000

    Refactors GET user to use routers

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,6 +1,10 @@
-const { getUsers } = require('../controllers/app.controllers');
+const { getUsers, getUserByUsername } = require('../controllers/app.controllers');
 const userRouter = require('express').Router();
 
 userRouter.get('/', getUsers)
+
+userRouter
+    .route('/:username')
+    .get(getUserByUsername)
 
 module.exports = userRouter;
